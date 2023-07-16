a2cfe665bee938d87e7118a888cb83704d435b99
nelson-jnrnd <nelsonjeanrenaud@gmail.com>
2023-07-16 11:02:21 +0200

    fixed plotting bug with limits

--- f1pitpred/plotting.py
+++ f1pitpred/plotting.py
@@ -56,8 +56,7 @@ def plot_race_predictions(x, y, proba, ax, color='cyan', marker='X', label='Pred
     ax : matplotlib Axes
         The axes with the plotted points
     """
-    ax.grid(True)
-    ax.hlines(y=0.5, xmin=0, xmax=x['TotalLaps'].max(), linewidth=1, colors='black', linestyles='dotted', zorder=0)
+    ax.hlines(y=0.5, xmin=x['LapNumber'].min(), xmax=x['TotalLaps'].max(), linewidth=1, colors='black', linestyles='dotted', zorder=0)
     indexes = np.where(y == 1)
     for idx in indexes:
         ax.vlines(x=idx+2, ymin=0, ymax=1, linewidth=1, colors='red', linestyles='dashed', zorder=0)
@@ -67,7 +66,7 @@ def plot_race_predictions(x, y, proba, ax, color='cyan', marker='X', label='Pred
     ax.set_xticks(np.arange(0, x['TotalLaps'].max(), 5))
     ax.set_ylim(0, 1)
     ax.set_ylabel('Probability of pitting')
-    ax.set_xlim(0, x['TotalLaps'].max())    
+    ax.set_xlim(x['LapNumber'].min(), x['TotalLaps'].max())    
 
     ax.set_title(title)
     return plot_remove_duplicate_legends(ax, loc='upper left', bbox_to_anchor=(1.01, 1), borderaxespad=0.)
@@ -94,7 +93,6 @@ def plot_stoppages(laps, ax):
     ax : matplotlib Axes
         The axes with the plotted stoppages
     """
-    ax.grid(True)
     for idx, lap in laps.iterrows():
         if lap['Red']:
             ax.axvspan(idx, idx+1, facecolor='red', alpha=0.3, zorder=0, label='Red Flag')
@@ -129,22 +127,21 @@ def plot_tyres(laps, ax):
     ax : matplotlib Axes
         The axes with the plotted tyre compounds
     """
-    ax.grid(True)
     for idx, lap in laps.iterrows():
         if lap['Compound_HARD']:
-            ax.axvspan(idx, idx+1, facecolor='red', alpha=1, zorder=-1, label='Soft', ymin=0.99)
+            ax.axvspan(idx-1, idx, facecolor='red', alpha=1, zorder=-1, label='Soft', ymin=0.99)
             #ax.axhline(y=0.99, xmin=idx, xmax=idx+1, linewidth=1, color='red', zorder=0)
         elif lap['Compound_MEDIUM']:
-            ax.axvspan(idx, idx+1, facecolor='yellow', alpha=1, zorder=-1, label='Medium', ymin=0.99)
+            ax.axvspan(idx-1, idx, facecolor='yellow', alpha=1, zorder=-1, label='Medium', ymin=0.99)
             #ax.axhline(y=0.99, xmin=idx, xmax=idx+1, linewidth=1, color='yellow', zorder=0)
         elif lap['Compound_SOFT']:
-            ax.axvspan(idx, idx+1, facecolor='grey', alpha=1, zorder=-1, label='Hard', ymin=0.99)
+            ax.axvspan(idx-1, idx, facecolor='grey', alpha=1, zorder=-1, label='Hard', ymin=0.99)
             #ax.axhline(y=0.99, xmin=idx, xmax=idx+1, linewidth=1, color='grey', zorder=0)
         elif lap['Compound_INTERMEDIATE']:
-            ax.axvspan(idx, idx+1, facecolor='green', alpha=1, zorder=-1, label='Intermediate', ymin=0.9)
+            ax.axvspan(idx-1, idx, facecolor='green', alpha=1, zorder=-1, label='Intermediate', ymin=0.9)
             #ax.axhline(y=0.99, xmin=idx, xmax=idx+1, linewidth=1, color='green', zorder=0)
         elif lap['Compound_WET']:
-            ax.axvspan(idx, idx+1, facecolor='blue', alpha=1, zorder=-1, label='Wet', ymin=0.99)
+            ax.axvspan(idx-1, idx, facecolor='blue', alpha=1, zorder=-1, label='Wet', ymin=0.99)
             #ax.axhline(y=0.99, xmin=idx, xmax=idx+1, linewidth=1, color='blue', zorder=0)
 
     return plot_remove_duplicate_legends(ax, loc='upper left', bbox_to_anchor=(1.01, 1), borderaxespad=0.)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='f1pitpred',
     packages=find_packages(),
-    version='0.1.8',
+    version='0.1.11',
     description='Predicting pitstops in F1',
     author='Nelson Jeanrenaud',
     license='MIT',
